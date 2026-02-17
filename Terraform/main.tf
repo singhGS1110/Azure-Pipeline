f522a8626a9db6d5334e409b342328265957281a
singhGS1110 <singhgurkiratgs11@gmail.com>
2026-02-17 12:12:45 +0530

    Refactor Terraform main.tf with variables and comments
    Updated the Terraform configuration to use variables for location and VM size. Added comments for better organization.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,11 +11,17 @@ provider "azurerm" {
   features {}
 }
 
+# -----------------------------
+# Resource Group
+# -----------------------------
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
-  location = "South India"
+  location = var.location
 }
 
+# -----------------------------
+# Virtual Network
+# -----------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "demo-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -23,35 +29,32 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# -----------------------------
+# Subnet (explicit dependency)
+# -----------------------------
 resource "azurerm_subnet" "subnet" {
   name                 = "demo-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.vnet]
 }
 
+# -----------------------------
+# Public IP (Standard must be Static)
+# -----------------------------
 resource "azurerm_public_ip" "pip" {
   name                = "demo-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "demo-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    public_ip_address_id          = azurerm_public_ip.pip.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
+# -----------------------------
+# Network Security Group
+# -----------------------------
 resource "azurerm_network_security_group" "nsg" {
   name                = "demo-nsg"
   location            = azurerm_resource_group.rg.location
@@ -82,20 +85,56 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# -----------------------------
+# Network Interface
+# -----------------------------
+resource "azurerm_network_interface" "nic" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [
+    azurerm_subnet.subnet,
+    azurerm_public_ip.pip
+  ]
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    public_ip_address_id          = azurerm_public_ip.pip.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# -----------------------------
+# Associate NSG to NIC
+# -----------------------------
 resource "azurerm_network_interface_security_group_association" "assoc" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
+# -----------------------------
+# Linux Virtual Machine
+# -----------------------------
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
+  computer_name       = "demovm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1ms"
+  size                = var.vm_size
+
   admin_username      = "azureuser"
   admin_password      = "Password1234!"
   disable_password_authentication = false
-  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  depends_on = [
+    azurerm_network_interface_security_group_association.assoc
+  ]
 
   os_disk {
     caching              = "ReadWrite"
@@ -110,9 +149,9 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
+# -----------------------------
+# Output Public IP
+# -----------------------------
 output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
-
-
-
