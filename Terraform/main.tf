54e56ebc0a5ba39f86dca44a6bc13ead49ccecea
singhGS1110 <singhgurkiratgs11@gmail.com>
2026-02-17 11:38:47 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -34,7 +34,9 @@ resource "azurerm_public_ip" "pip" {
   name                = "demo-pip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
@@ -111,3 +113,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
 output "public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
+
